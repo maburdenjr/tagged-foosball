c1820e3383b3c1374e9fde7afcf8a9d91bd3550b
Mike Burden <maburdenjr@gmail.com>
2015-08-05 22:28:37 -0700

    Game submission

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,43 @@
 
     var ifwe =  angular.module('app', []);
     ifwe.controller('ratingsSystem', ['$scope', '$http', ratingsSystem]);
+    ifwe.directive('deleteGame', deleteGame);
+
+    function deleteGame() {
+        return {
+            restrict: 'C',
+            link: function(scope, ele, attrs) {
+                ele.on('click', function(e) {
+                    var thisIndex = $(this).data('index');
+                    $(this).parent().parent().addClass('danger');
+                    var removeGame = confirm("Are you sure you want to delete this game?");
+                    if (removeGame == true) {
+                        scope.$apply(function () {
+                            scope.scores.splice(thisIndex, 1);
+                        });
+                    } else {
+                        $(this).parent().parent().removeClass('danger');
+                    }
+                });
+            }
+        }
+    }
 
     function ratingsSystem($scope, $http) {
 
         var addForm = $('#newGame');
 
-        $scope.scores = "Test";
+        $scope.scores;
+        $scope.formErrors = false;
+
+        $http.get('assets/data/scores.json')
+            .success(function(response) {
+                $scope.scores = response;
+                $scope.calculateScores();
+            })
+
+        $scope.calculateScores = function() {
+        }
 
         /* Validates the 'Add New Game' form to make sure the user is entering acceptable values */
         $scope.validateForm = function() {
@@ -36,7 +67,18 @@
         $scope.submitForm = function() {
             var hasErrors = $scope.validateForm();
             if (!hasErrors) {
-                console.log('You made it');
+                $scope.formErrors = false;
+                var scoreObj = {
+                    player1: addForm.find('#player1').val(),
+                    score1: addForm.find('#player1-score').val(),
+                    player2: addForm.find('#player2').val(),
+                    score2: addForm.find('#player2-score').val()
+                }
+                $scope.scores.push(scoreObj);
+                addForm.find('input').val(''); // Reset Form Values
+                console.log($scope.scores);
+            } else {
+                $scope.formErrors = true;
             }
         }
     }
